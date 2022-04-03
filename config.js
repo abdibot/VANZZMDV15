4027287ed4c5bb608fa30990e2d29972c4d2f962
abdibot <abdialamanah75@gmail.com>
2022-04-03 14:19:01 +0800

    VANZZMDV15

---
+++ config.js
@@ -0,0 +1,52 @@
+/**
+   * Error Chat Gua !!!
+   wa.me/67572919291
+*/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	zenz: 'https://zenzapi.xyz',
+}
+
+// Apikey Website Api
+global.APIKeys = {
+	'https://zenzapi.xyz': '5dcbf4f0fd',
+}
+
+// Other
+global.owner = ['6285824629954','6285824629954']
+global.web = 'https://m.youtube.com/channel/UCEDb6SkiPU0theI8J_WEZZA'
+global.bot = 'HELLO BOT'
+global.premium = ['6285824629954']
+global.packname = 'OWNER KU'
+global.author = 'WhatsApp Bot'
+global.sessionName = 'hisoka'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⭔'
+global.mess = {
+    success: '✓ Success',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: 'Loading...',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+global.limitawal = {
+    premium: "Infinity",
+    free: 100
+}
+global.thumb = fs.readFileSync('./media/kahfz.jpg')
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
